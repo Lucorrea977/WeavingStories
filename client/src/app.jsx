83d4b5f2268798a092e9fc0af9a9f5e53f5acbdf
Lucorrea977 <luucamila6@gail.com>
2024-05-27 20:24:56 -0300

    tailwind

---
+++ client/src/app.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+
+const App = () => {
+
+    return (
+        <div className='container mx-auto'>
+            <h1>Tailwind CSS</h1>
+            <h2>Incorporado con PostCss</h2>
+            <button className="btn btn-blue btn-blue:hover">Botón</button>
+           
+                
+            </div>
+       
+    )
+}
+
+export default App;
