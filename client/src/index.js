51101f9068a6cc0e031ca05f27101e723d76dece
Lucorrea977 <luucamila6@gail.com>
2024-06-06 13:57:23 -0300

    ...

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux';
-import store from './redux/store';
+
+import { Provider } from 'react-redux'
 import './assets/main.css';
 import App from './app';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Provider store={store}>
+      <Provider>
         <App />
-      </ Provider>
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
-  );
+);
